090cd5e5d37826489e6f724c6cdb98f31e8554bd
emediacje <emediacje@gmail.com>
2016-07-04 22:43:11 +0200

    Add Main.java file

---
+++ Main.java
@@ -0,0 +1,38 @@
+﻿import komunikacja.Komunikacja;
+
+public class Main {
+
+	static void test(int liczba, Komunikacja komunikacja) throws java.lang.Exception
+	{
+		switch(liczba)
+		{
+		case 1:
+			{
+				komunikacja.drukuj("(19, true, 60, 90, 150, 58, true, false, false, false, true)");
+				komunikacja.analizaWynikow(19, true, 60, 90, 150, 58, true, false, false, false, true);
+				break;
+			}
+		
+		case 2:
+		{
+			komunikacja.drukuj("(19, false, 60, 90, 150, 58, true, false, false, false, true)");
+			komunikacja.analizaWynikow(19, false, 60, 90, 150, 58, true, false, false, false, true);
+			break;
+		}
+
+			
+		}
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{	
+		Komunikacja komunikacja = new Komunikacja();
+		komunikacja.diagnozuj();
+
+//Test programu
+//		test(1,komunikacja);
+//		test(2,komunikacja);
+
+		
+	}
+}
